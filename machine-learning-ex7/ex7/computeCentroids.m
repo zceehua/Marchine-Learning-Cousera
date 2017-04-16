0ec00d625283a08cee10a1c0b2b8e9fc73a8953c
zceehua <zt136997015@hotmail.com>
2017-04-17 00:05:29 +0800

    Add files via upload

---
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -0,0 +1,49 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returs the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+[m n] = size(X);
+
+% You need to return the following variables correctly.
+centroids = zeros(K, n);%centriod的列数必须与X相同
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every centroid and compute mean of all points that
+%               belong to it. Concretely, the row vector centroids(i, :)
+%               should contain the mean of the data points assigned to
+%               centroid i.
+%
+% Note: You can use a for-loop over the centroids to compute this.
+%
+for k = 1:K,
+  v = [];%定义空矩阵
+  for i = 1:m,
+    if idx(i) == k,
+      v = [v; X(i,:)];%注意这里是如何运用矩阵求和的~！！
+    end
+  end
+  centroids(k,:) = sum(v)/size(v,1);
+end
+    
+
+
+
+
+
+
+
+% =============================================================
+
+
+end
+
---
+++ machine-learning-ex8/ex8/estimateGaussian.m
@@ -0,0 +1,44 @@
+function [mu sigma2] = estimateGaussian(X)
+%ESTIMATEGAUSSIAN This function estimates the parameters of a 
+%Gaussian distribution using the data in X
+%   [mu sigma2] = estimateGaussian(X), 
+%   The input X is the dataset with each n-dimensional data point in one row
+%   The output is an n-dimensional vector mu, the mean of the data set
+%   and the variances sigma^2, an n x 1 vector
+% 
+
+% Useful variables
+[m, n] = size(X);
+
+% You should return these values correctly
+mu = zeros(n, 1);
+sigma2 = zeros(n, 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the mean of the data and the variances
+%               In particular, mu(i) should contain the mean of
+%               the data for the i-th feature and sigma2(i)
+%               should contain variance of the i-th feature.
+%
+
+mu = mean(X);%求出每一列的平均值
+sigma2 = var(X,1);%求每一列的方差
+%for i=1:n
+%    mu(i)=(1/m)*sum(X(:,i))
+%    for j=1:m
+%        sigma2(i)=sigma2(i)+(X(j,i)-mu(i))^2
+%    end
+%    sigma2(i)=sigma2(i)/m
+%end
+
+
+
+
+
+
+
+
+% =============================================================
+
+
+end
